84ff7535e6d25811074c0c6a25da647e4c395336
salmaa-ibrahim <salmaa.ibrahimm19@gmail.com>
2026-09-18 11:11:10 +0300

    new

--- src/data/config.js
+++ src/data/config.js
@@ -1,15 +1,15 @@
 // src/data/config.js
 const config = {
   whatsapp: {
-    number: '+201556220922',
+    number: '+201102381018',
     message: 'Hello Vapesegy!'
   },
-  phone: '+201556220922',
+  phone: '+201102381018',
   social: {
-    instagram: 'https://instagram.com/vapesegy',
-    facebook: 'https://facebook.com/vapesegy',
-    whatsapp: 'https://wa.me/201556220922',
-    phone: 'tel:+201556220922'
+    instagram: 'https://instagram.com',
+    facebook: 'https://www.facebook.com/share/1BvKTgznLj/',
+    whatsapp: 'https://wa.me/201102381018',
+    phone: 'tel:+201102381018'
   },
   shipping: {
     deliveryTime: 'Same day delivery in Cairo',
